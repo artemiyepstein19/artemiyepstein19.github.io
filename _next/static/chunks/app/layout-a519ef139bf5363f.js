767b59bd42b78fe870538cf2fdb730f28ac928ae
artemiyepstein19 <artibald19@gmail.com>
2026-08-01 19:47:44 +0200

    Complete public offer, Website Sprint, and privacy release gates (#1)
    Publish approved public-claim corrections, Website Sprint page, and Privacy/Formspree updates.

--- _next/static/chunks/app/layout-a519ef139bf5363f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{64:()=>{},2525:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,64,23))}},e=>{e.O(0,[701,98,376,358],()=>e(e.s=2525)),_N_E=e.O()}]);
